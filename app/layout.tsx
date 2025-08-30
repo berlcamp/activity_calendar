fe6ef2e9f4e564b15420f82b26380c806fb8f080
Berl treasure Campomanes <berlcamp@gmail.com>
2025-08-30 22:50:32 +0800

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'AO Calendar',
-  description: 'AO Calendar'
+  title: 'Admin App',
+  description: 'Admin App'
 }
 
 export default function RootLayout({
